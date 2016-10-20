8cdeb66730a5f3d37d24b28ee9fdd624062d7eff
Maxwell Cady and Suzi Rubino <maxwell@>
2016-10-20 11:15:39 -0700

    update if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 $(function() {
-  $("form#beliefs ").submit(function(event) {
+  $("form#beliefs").submit(function(event) {
     var wallInput = parseInt($("select#wall").val());
     var emailInput = parseInt($("select#email").val());
     var putinInput = parseInt($("select#putin").val());
 
-    console.log()
-
-    if (wallInput + emailInput + putinInput > 1) {
+    if (!wallInput || !emailInput || !putinInput) {
+      alert("Please answer each question");
+    } else if (wallInput = 2) {
       alert("You should NOT vote");
+    } else if (wallInput + emailInput + putinInput < 3) {
+        alert("You should NOT vote");
     } else {
-      alert("You should vote for Hillary");
-    }
+        alert("You should vote for Hillary");
+      }
+
 
     event.preventDefault();
   });
